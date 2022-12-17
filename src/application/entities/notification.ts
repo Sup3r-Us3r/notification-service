16d48410e21199b3cbfad39604efb1aed8484787
Sup3r-Us3r <maydersonmello@gmail.com>
2022-12-16 21:23:17 -0300

    create notification and content entity

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,83 @@
+import { randomUUID } from 'node:crypto';
+
+import { Replace } from '@helpers/replace';
+import { Content } from './content';
+
+interface INotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  canceledAt?: Date | null;
+  createdAt: Date;
+}
+
+class Notification {
+  private _id: string;
+  private props: INotificationProps;
+
+  constructor(
+    props: Replace<INotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unread() {
+    this.props.readAt = null;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
+  }
+
+  public get canceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
+  }
+
+  public set createdAt(createdAt: Date) {
+    this.props.createdAt = createdAt;
+  }
+}
+
+export { INotificationProps, Notification };
